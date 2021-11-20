b8baa0e1e8abf82a384f3ebe7ea2c208db1b8fe0
Odbayar <odbayar94@gmail.com>
2021-11-20 09:27:19 +0800

    post save Pending by default

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -8,7 +8,7 @@ const PostSchema = new Schema(
 
     status: {
       type: String,
-      default: "Draft",
+      default: "Pending",
     },
     title: { type: String, required: true },
     context: { type: String, required: true },
